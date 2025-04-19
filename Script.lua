c3c681b349d42f9bbfb02938fec77f23fd9487a0
Trmx01 <140913985+Trmx01@users.noreply.github.com>
2025-04-19 16:45:57 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2,6 +2,25 @@ local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 
 -- window
 local window = DrRayLibrary:Load("Zizu", "Default")
+
 -- prima tab
 local tab1 = DrRayLibrary.newTab("Auto Bubble", "")
-tab1.newToggle("Toggle", "Toggle! (prints the state)", true)
+
+-- variabile per fermare/attivare il loop
+local autoBubbleEnabled = false
+
+-- funzione per far scoppiare la bolla
+local function blowBubble()
+    local args = {
+        [1] = "BlowBubble"
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer(unpack(args)) 
+end
+
+-- toggle
+tab1.newToggle("Auto Bubble", "Auto farm Bubble", true, function(toggleState)
+    autoBubbleEnabled = toggleState
+    while autoBubbleEnabled and task.wait(0.1) do
+        pcall(blowBubble)
+    end
+end)
